7f9eb8898a93b9cf3b415fd728f8e0366fd45dac
Nafis Abdullah Bin Azad <nafis.abdullah_bin_azad@kcl.ac.uk>
2018-01-17 23:17:14 +0000

    Add UI and Add Win and PickUp Count Text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 
     private Rigidbody2D rb2d;
     public float speed;
+    private int count;
+    public Text countText;
+    public Text winText;
 
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-    }
+        count = 0;
+        SetCountText();
+    } 
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -26,7 +32,18 @@ public class PlayerController : MonoBehaviour {
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
         }
         
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 9)
+        {
+            winText.text = "You Won!!!";
+        }
     }
 }
